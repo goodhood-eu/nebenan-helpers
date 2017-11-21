c25446057eccfbb9983bc1e12e5dbff67a2c4fa2
Max Degterev <max@degterev.me>
2017-11-21 14:24:39 +0100

    cleanup

--- require_hooks.js
+++ require_hooks.js
@@ -1,7 +1,2 @@
 require('babel-register')({ extensions: ['.es'] });
-const CoffeeScript = require('coffeescript');
-
-const { compile } = CoffeeScript;
-const overrides = { transpile: JSON.parse(require('fs').readFileSync(`${__dirname}/.babelrc`)) };
-CoffeeScript.compile = (file, options) => compile(file, Object.assign(options, overrides));
-CoffeeScript.register();
+require('coffeescript/register');
