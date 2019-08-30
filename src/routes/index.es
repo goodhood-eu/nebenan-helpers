3d1ef2c7d632323c1c8996da91e495c723f5201a
Max Degterev <max@degterev.me>
2019-08-30 17:30:41 +0200

    added new route utils for validations

--- src/routes/index.es
+++ src/routes/index.es
@@ -1,4 +1,27 @@
+import memoize from 'lodash/memoize';
 import { stringify, parse } from 'qs';
+
+const REGEX_INT = '\\d+';
+const REGEX_SLUG = '[\\w-]+';
+
+export const validations = {
+  id: REGEX_INT,
+  hoodId: REGEX_INT,
+  zipCode: '\\d{5}',
+  token: '[\\w-]{20}',
+  accessCode: '[\\w]{5}-[\\w]{5}',
+  slug: REGEX_SLUG,
+  hood: REGEX_SLUG,
+  stringId: '\\w{6,}',
+};
+
+export const getParamReplacement = memoize((fragment, param) => {
+  const regex = validations[param];
+  if (!regex) return fragment;
+  return `${fragment}(${regex})`;
+});
+
+export const getValidatedPath = (path) => path.replace(/:(\w+)/g, getParamReplacement);
 
 export const getQuery = ({ search }) => parse(search.substr(1));
 export const getSearch = (query, prefix = '?') => {
--- test/routes/index.js
+++ test/routes/index.js
@@ -1,6 +1,10 @@
 const { assert } = require('chai');
 
 const {
+  validations,
+  getParamReplacement,
+  getValidatedPath,
+
   getQuery,
   getSearch,
   getPage,
@@ -9,6 +13,67 @@ const {
 
 
 describe('routes', () => {
+  it('validations', () => {
+    const isInt = [
+      '123',
+    ];
+
+    const isNotInt = [
+      '12asd3',
+      '123.78',
+    ];
+
+    const getTest = (regex) => (value) => (new RegExp(`^${regex}$`)).test(value);
+
+    isInt.forEach((item) => assert.isTrue(getTest(validations.id)(item), `Passed for ${item}`));
+    isNotInt.forEach((item) => assert.isFalse(getTest(validations.id)(item), `Failed for ${item}`));
+
+    const isToken = [
+      'a'.repeat(20),
+      'abcSDASD123toikg2345',
+    ];
+
+    const isNotToken = [
+      'a'.repeat(19),
+      'abcS-ASD123toikg2$45',
+    ];
+
+    isToken.forEach((item) => assert.isTrue(getTest(validations.token)(item), `Passed for ${item}`));
+    isNotToken.forEach((item) => assert.isFalse(getTest(validations.token)(item), `Failed for ${item}`));
+
+    const isAccessCode = [
+      'a123b-zxcZA',
+    ];
+
+    const isNotAccessCode = [
+      'a123b-zxcA',
+      'a123b-zxcA$',
+      'a123bAzxcAz',
+    ];
+
+    isAccessCode.forEach((item) => assert.isTrue(getTest(validations.accessCode)(item), `Passed for ${item}`));
+    isNotAccessCode.forEach((item) => assert.isFalse(getTest(validations.accessCode)(item), `Failed for ${item}`));
+  });
+
+  it('getParamReplacement', () => {
+    const result = getParamReplacement(':id', 'id');
+
+    assert.isTrue(/^:id(.+)$/.test(result), 'param replaced correctly');
+    assert.equal(getParamReplacement(':id', 'id'), result, 'repeated params replacements work');
+
+    assert.equal(getParamReplacement(':unknown', 'unknown'), ':unknown', 'unknown params passed through');
+    assert.equal(getParamReplacement(':unknown', 'unknown'), ':unknown', 'repeated unknown calls work');
+  });
+
+  it('getParamReplacement', () => {
+    assert.isTrue(/^\/test\/:id(.+)$/.test(getValidatedPath('/test/:id')), 'param replaced correctly');
+    assert.isTrue(/^\/test\/:id(.+)?$/.test(getValidatedPath('/test/:id?')), 'optional param replaced correctly');
+    assert.isTrue(/^\/test\/:id(.+)*$/.test(getValidatedPath('/test/:id*')), 'repeated param replaced correctly');
+
+    assert.equal(getValidatedPath('/no/params'), '/no/params', 'paths without params work ok');
+    assert.equal(getValidatedPath('/no/:param'), '/no/:param', 'unknown params passed through');
+  });
+
   it('getQuery', () => {
     assert.deepEqual(getQuery({ search: '' }), {}, 'empty');
     assert.deepEqual(getQuery({ search: '?' }), {}, 'empty 2');
