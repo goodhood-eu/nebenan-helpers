9bc5fa87895d3d7a9c23d64ec1debfd4bd6035a9
nomn <i.m.nomn@gmail.com>
2021-10-07 17:29:40 +0200

    [CORE-8286] utils jsdoc

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,11 @@
 const unsafeFileCharsRegex = /[\\/:*?"'<>|]/g;
 
+/**
+ * @function
+ * @param {string} path
+ * @param {string} fullPath
+ * @return {boolean}
+ */
 export const isPathActive = (path, fullPath) => {
   const pathChunks = path.split('/');
   const fullPathChunks = fullPath.split('/');
@@ -8,15 +14,39 @@ export const isPathActive = (path, fullPath) => {
   return pathChunks.join('/') === fullPathChunks.join('/');
 };
 
+/**
+ * @function
+ * @param {string} filename
+ * @return {string}
+ */
 export const escapeFileName = (filename) => filename.replace(unsafeFileCharsRegex, '_');
+
+/**
+ * @function
+ * @param {function} fn
+ * @param args
+ * @return {*}
+ */
 export const invoke = (fn, ...args) => {
   if (typeof fn === 'function') return fn(...args);
 };
 
+/**
+ * @function
+ * @param {object} context
+ * @param {function} fn
+ * @param args
+ * @return {*}
+ */
 export const invokeOn = (context, fn, ...args) => {
   if (context && typeof fn === 'function') return fn.call(context, ...args);
 };
 
+/**
+ * @function
+ * @param {object} context
+ * @param funcs
+ */
 export const bindTo = (context, ...funcs) => funcs.forEach((func) => {
   if (context[func]) context[func] = context[func].bind(context);
 });
