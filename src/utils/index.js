5f702e7cdbbf8fdd88e190aa8093717c6c0d69b7
Uche Jude <iamuchejude@gmail.com>
2022-03-29 06:04:42 +0100

    Add isMobileTouchDevice() util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 const unsafeFileCharsRegex = /[\\/:*?"'<>|]/g;
+const UARegex = /(\b(BlackBerry|webOS|iPhone|IEMobile)\b)|(\b(Android|Windows Phone|iPad|iPod)\b)/i;
 
 /**
  * @function
@@ -50,3 +51,23 @@ export const invokeOn = (context, fn, ...args) => {
 export const bindTo = (context, ...funcs) => funcs.forEach((func) => {
   if (context[func]) context[func] = context[func].bind(context);
 });
+
+/**
+ * @function isMobileTouchDevice
+ * @description Checks if user client is a mobile device
+ * @returns {boolean}
+ */
+export const isMobileTouchDevice = () => {
+  const touchPoints = ['maxTouchPoints', 'msMaxTouchPoints'];
+
+  const predicate = (tp) => tp in navigator && navigator[tp] > 0;
+  const hasTouchPoints = touchPoints.some(predicate);
+  if (!hasTouchPoints) return false;
+
+  const hasPointerDevice = window.matchMedia?.('(pointer:coarse)').matches;
+  if (!hasPointerDevice) return false;
+
+  if (!('orientation' in window)) return false;
+
+  return UARegex.test(navigator.userAgent);
+};
